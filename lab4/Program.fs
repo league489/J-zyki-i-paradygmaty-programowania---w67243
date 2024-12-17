49e700e9d1bb7806c0122f99133bd03c32225e3f
league489 <107813326+league489@users.noreply.github.com>
2024-12-17 22:09:04 +0100

    Update Program.fs

--- lab4/Program.fs
+++ lab4/Program.fs
@@ -4,31 +4,6 @@ open z3
 open z4
 open z5
 open System
-//    else 
-//         "y jest większy"
-        
-//let listaA = [1;2;3;4]
-//let listaB = 0:: listaA
-
-////for item in listaA do
-////    printf "%d " item
-
-//let listaC = listaA @ listaB
-
-//for item in listaC do
-//    printf "%d " item
-
-//type Osoba ={
-//    Imie: string;
-//    Wiek: int;  }
-//let sosba1 = {Imie = "Jan"; Wiek = 12}
-//let osoba2 = {Imie = "Janina"; Wiek = 30}
-//let krotka = (1, "ala", true)
-
-
-
-
-
 
 // Rozpoczęcie gry
 [<EntryPoint>]
